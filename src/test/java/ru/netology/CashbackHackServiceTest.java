46dbe37b69caf284ee2ceb2353f36469ec53cfae
Yuliya.S <yuliyamarketplace@yandex.ru>
2024-11-02 16:14:51 +0400

    find the error

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -2,7 +2,8 @@ package ru.netology;
 
 import org.junit.Test;
 import org.junit.Assert;
-import static org.testng.AssertJUnit.assertEquals;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
